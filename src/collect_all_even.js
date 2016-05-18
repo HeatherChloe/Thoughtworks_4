438cef61eb347e2353d4acaa23bafd4cce41605f
root <root@localhost.localdomain>
2016-05-18 16:34:07 +0800

    hello

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -2,13 +2,17 @@ function collect_same_elements(collection_a, object_b) {
     var a = collection_a;
     var b = object_b;
     var rst = [];
-    for(var i = 0; i < b.value.length; ++i){
-        for(var j = 0; j < a.length; ++j){
-            if(a[j].key == b.value[i]){
-                rst.push(a[j].key);
+    var i = 0;
+    for(x in a)
+    {
+        for(i = 0; i < b.value.length; ++i)
+        {
+            if(a[x].key == b.value[i])
+            {
+                 rst.push(a[x].key);
             }
         }
     }
-    return rst;
+     return(rst);
 }
 module.exports = collect_same_elements;
